0071086fc762c24bde5763fb936871bd90b1a9b8
kripi-png <66375160+kripi-png@users.noreply.github.com>
2024-05-16 13:07:52 +0300

    chore: rewrite cache.ts comments for clarity

--- backend/src/cache.ts
+++ backend/src/cache.ts
@@ -7,19 +7,29 @@ export const checkCacheMiddleware =
 	(cacheScope: string, keyName: string) =>
 	async (req: Request, res: Response, next: NextFunction) => {
 		/*
-			checkCacheMiddleware returns a middleware that handles
-			checking cache for cacheScope:key. If such entry exists
-			and it has not expired, the entry is returned.
-			otherwise, next() is called and the next handler takes
-			care of further actions (db queries etc.)
+		Returns a middleware that handles checking cache for given scope and key.
+		Checking cache can be disabled with DISABLE_CACHE env variable
+		keyName is used to find the right value from req.params
+
+		For example, checkCacheMiddleware('products', 'barcode') on route /products/:barcode
+		returns a function that checks Redis for 'products:<req.params['barcode']>',
+		
+		If the entry has not expired, data is returned
+		Otherwise next() is called to let next handler take care of the request
+		
+		NOTE: expiring is handled by Redis internally using EXPIRE command
+		when caching the data
+		More info: https://redis.io/docs/latest/commands/expire/
+		
+		Roope Sinisalo, 2024
 		*/
 
 		if (process.env?.DISABLE_CACHE) {
-			console.warn('Cache is disabled due to environment variable');
+			console.warn('Cache is disabled with env variable');
 			return next();
 		}
 
-		// e.g. products:4047443419439
+		// e.g. products:80177173
 		const key = cacheScope + ':' + req.params[keyName];
 		const product = await redis.hgetall(key);
 		// if data does not exist go to next middleware
@@ -27,3 +37,5 @@ export const checkCacheMiddleware =
 
 		res.status(200).json(product);
 	};
+
+
